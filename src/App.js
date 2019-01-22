00f3d09717d14ee5c0d5ca2cacb8928e21f45e17
Michael Oelkers <moelkers2010@gmail.com>
2019-01-22 16:43:45 -0600

    adds props functionality to person

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a react app, now on git.</h1>
         <p>Just a little paragraph here, minding my own business.</p>
-        <Person></Person>
-        <Person></Person>
-        <Person></Person>
+        <Person name="Max" age="28"></Person>
+        <Person name="Manu" age="29">My Hobbies: Racing</Person>
+        <Person name="Stephanie" age="26"></Person>
         
       </div>
       
