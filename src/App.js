6b5664372f57d3d140cc20a76c3190b12bd0a371
Michael Oelkers <moelkers2010@gmail.com>
2019-01-22 16:17:20 -0600

    Just meddling around with JSX tags.

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi, I'm a react app, now on git.</h1>
+        <p>Just a little paragraph here, minding my own business.</p>
       </div>
+      
     );
+    //return React.createElement('div',{className: 'App'}, React.createElement('h1',null,'Does this work?'))
   }
 }
 
