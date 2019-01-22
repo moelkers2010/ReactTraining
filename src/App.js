0e7127d5ad6e59e28dc28750412789d75b9f6720
Michael Oelkers <moelkers2010@gmail.com>
2019-01-22 17:15:20 -0600

    creates click functionality on button.

--- src/App.js
+++ src/App.js
@@ -11,13 +11,16 @@ class App extends Component {
     ]
   }
 
+  switchNameHandler = () =>{
+    console.log('Hey, clicks work.')
+  }
 
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a react app, now on git.</h1>
         <p>Just a little paragraph here, minding my own business.</p>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
