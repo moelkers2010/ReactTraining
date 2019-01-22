92d8ca3fa1254b21ef8dae0dc43b860a0c8284c9
Michael Oelkers <moelkers2010@gmail.com>
2019-01-22 17:11:03 -0600

    sets up page using state for data.

--- src/App.js
+++ src/App.js
@@ -18,9 +18,9 @@ class App extends Component {
         <h1>Hi, I'm a react app, now on git.</h1>
         <p>Just a little paragraph here, minding my own business.</p>
         <button>Switch Name</button>
-        <Person name="Max" age="28"></Person>
-        <Person name="Manu" age="29">My Hobbies: Racing</Person>
-        <Person name="Stephanie" age="26"></Person>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
         
       </div>
       
