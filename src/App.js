1b0d3f1e4292f5429ffb9b487d4dada1ab3486bc
Michael Oelkers <moelkers2010@gmail.com>
2019-01-22 16:26:58 -0600

    Creates Person element with text.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
@@ -7,6 +8,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a react app, now on git.</h1>
         <p>Just a little paragraph here, minding my own business.</p>
+        <Person></Person>
       </div>
       
     );
