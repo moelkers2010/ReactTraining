32328f2c4b9719528cde9a2f04e004952e1b3e63
Michael Oelkers <moelkers2010@gmail.com>
2019-01-23 16:54:16 -0600

    Adds arrow function return functionality to onclick handler.

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () =>{
+  switchNameHandler = (newName) =>{
     //console.log('Hey, clicks work.')
     // This is a NO NO this.state.persons[0].name = 'Dr. Evil'
     this.setState({persons: [
-      {name: 'Dr. Evil', age: 28},
+      {name: newName, age: 28},
       {name: 'Manu', age: 29},
       {name: 'Stephanie', age: 27}
     ]})
@@ -26,8 +26,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a react app, now on git.</h1>
         <p>Just a little paragraph here, minding my own business.</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
+        <button onClick={() => this.switchNameHandler('Dr. Evil')}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}
+        click={this.switchNameHandler.bind(this, 'Bad Horse')}></Person>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
         
